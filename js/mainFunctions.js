7c80989d4c3c1a7011ef30c72755e3dfc94a27fd
Aneesh Tigga <aneeshtigga@gmail.com>
2021-08-21 17:32:56 +0530

    Interview task for Jio

---
+++ js/mainFunctions.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+    console.log('ready');
+    $('.header_wraper').load('./components/header.html');
+    $('.navbar_wraper').load('./components/navbar.html');
+    $('.dash_wraper').load('./components/dash.html');
+    $('.footer_wraper').load('./components/footer.html');
+    
+});
